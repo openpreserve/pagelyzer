ef9e85f71a64464d2f3df046c6d893c8f3bd38bc
asanoja <afsanoja@gmail.com>
2014-01-20 16:32:42 +0100

    adding headers and comments

--- java/fr/lip6/jpagelyzer/CaptureResult.java
+++ java/fr/lip6/jpagelyzer/CaptureResult.java
@@ -1,7 +1,40 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+# Andrés Sanoja
+# UPMC - LIP6
+# pagelyzer 
+#
+# Copyright (C) 2011, 2012, 2013, 2014 Andrés Sanoja, Université Pierre et Marie Curie -
+# Laboratoire d'informatique de Paris 6 (LIP6)
+#
+# Authors
+# Andrés Sanoja andres.sanoja@lip6.fr
+# Alexis Lechervy alexis.lechervy@lip6.fr
+# Zeynep Pehlivan zeynep.pehlivan@lip6.fr
+# Myriam Ben Saad myriam.ben-saad@lip6.fr
+# Marc Law marc.law@lip6.fr
+# Carlos Sureda carlos.sureda@lip6.fr
+# Jordi Creus jordi.creus@lip6.fr
+# LIP6 / Université Pierre et Marie Curie
+
+# Responsables WP
+# Matthieu CORD/UPMC
+# Stéphane GANÇARSKI/UPMC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Some parts of this package are adapted from the BrowserShot proyect developed by IM, France.
+# https://github.com/sbarton/browser-shot-tool-mapred
  */
 
 package fr.lip6.jpagelyzer;
