59979957667c1931970209ab915c6a0668880e77
ZeynepP <zeynep.pehlivan@lip6.fr>
2014-05-09 17:41:56 +0200

    Organize imports

--- JKernelMachines/src/main/java/JKernelMachines/fr/lip6/kernel/IndexedKernel.java
+++ JKernelMachines/src/main/java/JKernelMachines/fr/lip6/kernel/IndexedKernel.java
@@ -1,13 +1,8 @@
 package JKernelMachines.fr.lip6.kernel;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
-@SuppressWarnings("static-access")
+
 public class IndexedKernel {
 	/**
 	 * 
